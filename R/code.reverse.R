726bf38ebe235c6ce62ee322646368a772971afa
Aaron0696 <52684909+Aaron0696@users.noreply.github.com>
2020-09-18 11:47:26 +0800

    Add files via upload

--- R/code.reverse.R
+++ R/code.reverse.R
@@ -1,8 +1,13 @@
 # reverse codes a vector using factor()
 # 1,2,3,4,5 -> 5,4,3,2,1
 code.reverse <- function(vector, original_levels){
+  # convert vector input to a factor with the original levels
   vector <- factor(vector, levels = original_levels)
+  # reverse code values in the vector by flipping the order of the vector
   levels(vector) <- levels(vector)[length(levels(vector)):1]
+  # return the levels to the original without changing values in the vector
+  vector <- factor(vector, levels = original_levels)
+  # return
   return(vector)
 }
 # vector <- c("SA","A","D","SD")
