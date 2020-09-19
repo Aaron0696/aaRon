584a1ae506791219bedc0fd5d846a5ecb0e3345d
Aaron Lim <aronlimjm@gmail.com>
2020-09-19 14:36:29 +0800

    Implemented with roxygen instead.

--- R/code.reverse.R
+++ R/code.reverse.R
@@ -1,5 +1,16 @@
-# reverse codes a vector using factor()
-# 1,2,3,4,5 -> 5,4,3,2,1
+#' Reverse Coding of Scales
+#'
+#'Implements reverse-coding of items using \code{factor()}. One advantage over \code{psych::reverse.code} is that input need not be numeric.
+#'
+#' @param vector A numeric, character or factor vector.
+#' @param original_levels A character vector denoting the order of the original scale (lowest level on the left)
+#'
+#' @return Returns a factor vector of the same length as vector with reverse-coded responses, with levels corresponding to original_level
+#' @export
+#'
+#' @examples
+#' mydata <- data.frame(myscale = c("Disagree", "Neutral", "Agree", "Neutral","Agree"))
+#' code.reverse(vector = mydata$myscale, original_levels = c("Disagree","Neutral","Agree"))
 code.reverse <- function(vector, original_levels){
   # convert vector input to a factor with the original levels
   vector <- factor(vector, levels = original_levels)
@@ -10,6 +21,3 @@ code.reverse <- function(vector, original_levels){
   # return
   return(vector)
 }
-# demonstration
-# vector <- c("SA","A","D","SD")
-# code.reverse(vector, original_levels = c("SD","D","A","SA"))
