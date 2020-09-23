83ea9ec812b8acaca0c1aecb6654c17ab687ea78
Aaron Lim <aronlimjm@gmail.com>
2020-09-23 18:00:27 +0800

    Added new argument align and dp.

--- R/prettyalpha.R
+++ R/prettyalpha.R
@@ -5,12 +5,14 @@
 #'
 #' @param alphaobj Summary object from executing psych::alpha().
 #' @param ... Additional arguments passed onto \code{kable()}.
+#' @param dp Defaults to 3. Number of decimal points for all numeric values.
+#' @param align Defaults to "c", which stands for centered. Adjusts the alignment of text within table cells. Works the same way as \code{kable()}.
 #'
 #' @return
 #' @export
 #'
 #' @examples To be added...
-prettyalpha <- function(alphaobj, ...)
+prettyalpha <- function(alphaobj, dp = 2, align = "c", ...)
 {
   # for each of these dataframes within alphaobj, total, alpha.drop and item.stats
   # use kable() to print out a nice table for each
@@ -18,16 +20,16 @@ prettyalpha <- function(alphaobj, ...)
   cat("**Cronbach Alpha**:")
   cat("\n\n")
   names(alphaobj$total) <- toupper(names(alphaobj$total))
-  print(knitr::kable(alphaobj$total,align = "c",...))
+  print(knitr::kable(alphaobj$total,align = align,digits = dp,...))
   cat("\n\n")
   cat("**Alpha Values If Certain Items Were Dropped**:")
   cat("\n\n")
   names(alphaobj$alpha.drop) <- toupper(names(alphaobj$alpha.drop))
-  print(knitr::kable(alphaobj$alpha.drop, row.names = TRUE, align = "c", ...))
+  print(knitr::kable(alphaobj$alpha.drop, row.names = TRUE, align = align, digits = dp, ...))
   cat("\n\n")
   cat("**Item-Level Statistics**:")
   cat("\n\n")
   names(alphaobj$item.stats) <- toupper(names(alphaobj$item.stats))
-  print(knitr::kable(alphaobj$item.stats, row.names = TRUE, align = "c", ...))
+  print(knitr::kable(alphaobj$item.stats, row.names = TRUE, align = align, digits = dp, ...))
   cat("\n\n")
 }
