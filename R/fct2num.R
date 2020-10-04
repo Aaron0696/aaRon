25aac294b9684494446b101d3fdfa093598e21fe
Aaron Lim <aronlimjm@gmail.com>
2020-10-04 19:20:29 +0800

    Added checks for non-factor inputs and printing suppression if there are 15 or more levels.

--- R/fct2num.R
+++ R/fct2num.R
@@ -6,12 +6,24 @@
 #' @export
 #'
 #' @examples
-#' a <- factor(c("Agree","Disagree","Agree"))
+#' a <- factor(c("Agree","Disagree","Agree","Agree","Disagree"))
 #' fct2num(a)
 fct2num <- function(factor_vector)
 {
-  # print out levels before conversion
-  cat("Levels:", levels(factor_vector), "\n")
-  # convert to numeric
-  return(as.numeric(factor_vector))
+  # check if the input is a vector
+  if(is.factor(factor_vector))
+  {
+    # print out levels before conversion
+    if(length(levels(factor_vector)) < 20)
+    {
+      cat("Levels:", levels(factor_vector), "\n")
+    } else {
+      cat("More than 15 levels, printing suppressed.\n")
+    }
+    # convert to numeric
+    return(as.numeric(factor_vector))
+  } else
+  {
+   stop("Input not a factor.")
+  }
 }
