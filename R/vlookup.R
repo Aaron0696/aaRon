7685b161983836f78ae8d4b899144b19f3956015
Aaron Lim <aronlimjm@gmail.com>
2020-10-05 15:09:58 +0800

    Added sorting mechanism in vlookup().

--- R/vlookup.R
+++ R/vlookup.R
@@ -21,9 +21,17 @@
 #'         lookup_value = "Magnitude")
 vlookup <- function(data,lookup_table,data_col,lookup_col,lookup_value)
 {
-  merge(x = data,y = lookup_table[,c(lookup_col,lookup_value)],
+  # create an index such that we can resort the dataframe back into order after merge()
+  # o571 is just a random column name... so that there will be no duplicate columns
+  data$o571 <-1:nrow(data)
+  out <- merge(x = data,y = lookup_table[,c(lookup_col,lookup_value)],
         by.x = data_col, by.y = lookup_col,
         all.x = TRUE)
+  # sort
+  out <- out[order(out$o571),]
+  # remove index column
+  out <- out[,-grep("^o571$",names(out))]
+  return(out)
 }
 # for testing purposes...
 # data <- data.frame(Type = rep(1:3,5), b = 4:6)
