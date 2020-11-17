987fe7fe7e6937d0bf08d7709fcfcc416b472ab3
Aaron Lim Jin Ming <fasaljm@nus.edu.sg>
2020-11-17 10:14:22 +0800

    Renamed aggregate.scale to aggregateScale to fix some bugs.

--- R/aggregateScale.R
+++ R/aggregateScale.R
@@ -12,7 +12,7 @@
 #' mydata <- data.frame(a = c(1:8,NA,NA), b = 1:10, c = rep(NA,10))
 #' aggregate.scale(mydata)
 #' aggregate.scale(mydata, NA.threshold = 0.4)
-aggregate.scale <- function(dataframe,aggregate = "SUM",NA.threshold = 0.5,na.rm = TRUE)
+aggregateScale <- function(dataframe,aggregate = "SUM",NA.threshold = 0.5,na.rm = TRUE)
 {
   # calculate rowMeans or rowSums
   if(aggregate == "SUM")
