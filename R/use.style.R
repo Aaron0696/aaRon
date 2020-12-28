1306fb765669e33986ccf3810b78b59fc64d0ffa
Aaron Lim Jin Ming <fasaljm@nus.edu.sg>
2020-12-28 20:29:54 +0800

    Added option of using aaron.casual.css.

--- R/use.style.R
+++ R/use.style.R
@@ -2,15 +2,25 @@
 #'
 #' Function to apply cascading stylesheets from \code{aaRon} to any Rmarkdown document. Also accessible through the templates provided in the package.
 #'
+#' @param mode Defaults to "work", can take the option of "casual". Switches between CSS themes for work and casual use. The casual theme is slightly more flashy.
+#'
+#'
 #' @return
 #' @export
 #'
 #' @examples
 #' aaRon::use.style() # include into any R code chunk
-use.style <- function()
+#' aaRon::use.style(mode = "casual")
+use.style <- function(mode = "work")
 {
   # path to css file
-  mypath <- system.file("stylesheets","aaRon.css", package = "aaRon")
+  if(mode == work)
+  {
+    mypath <- system.file("stylesheets","aaRon.css", package = "aaRon")
+  } else {
+    mypath <- system.file("stylesheets","aaRon.casual.css", package = "aaRon")
+  }
+
   # include CSS in html output
   htmltools::includeCSS(mypath)
 }
