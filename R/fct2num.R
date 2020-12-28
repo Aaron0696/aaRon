1bd4be4b853332d5299ae137a1d9142ec33b8802
Aaron Lim Jin Ming <fasaljm@nus.edu.sg>
2020-12-28 12:09:14 +0800

    Added option to use cat() or message() for printing the levels of the factor.

--- R/fct2num.R
+++ R/fct2num.R
@@ -5,6 +5,7 @@
 #'
 #' @param factor_vector A vector that is a factor.
 #' @param start Defaults to 1. The starting value of the resultant numeric vector. The value inputted for \code{start} will be the lowest numeric value for the lowest factor level.
+#' @param print Defaults to "message". Determines if the text messages of this function uses \code{cat()} or \code{message()}. This affects the behaviour in specific Rmarkdown chunks.
 #'
 #' @return
 #' @export
@@ -12,7 +13,8 @@
 #' @examples
 #' a <- factor(c("Agree","Disagree","Agree","Agree","Disagree"))
 #' fct2num(a)
-fct2num <- function(factor_vector, start = 1)
+#' fct2num(a, start = 0) # if likert scales begin at 0
+fct2num <- function(factor_vector, start = 1, print = "message")
 {
   # check if the input is a vector
   if(is.factor(factor_vector))
@@ -20,9 +22,21 @@ fct2num <- function(factor_vector, start = 1)
     # print out levels before conversion
     if(length(levels(factor_vector)) < 20)
     {
-      message("Levels: ", paste0(levels(factor_vector), collapse = ", "), "\n")
+      if(print == "message")
+      {
+        message("Levels: ", paste0(levels(factor_vector), collapse = ", "))
+      } else
+      {
+        cat("Levels:", paste0(levels(factor_vector), collapse = ", "), "\n")
+      }
     } else {
-      message("More than 15 levels, printing suppressed.\n")
+      if(print == "message")
+      {
+        message("More than 15 levels, printing suppressed.")
+      } else
+      {
+        cat("More than 15 levels, printing suppressed.\n")
+      }
     }
   } else {
     warning("Input is not a factor.")
