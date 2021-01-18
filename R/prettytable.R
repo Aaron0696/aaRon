9af59e780cc202937418e5dc6a2a2a69464dcb14
Aaron Lim Jin Ming <fasaljm@nus.edu.sg>
2021-01-18 18:43:37 +0800

    Added new function prettytable().

---
+++ R/prettytable.R
@@ -0,0 +1,43 @@
+#' Printing 1 & 2 Way Frequency Tables With Relative Proportions
+#'
+#' Automatically append relative proportions/percentages to raw counts calculated from \code{table()}. Currently only works for 1 and 2 way frequency tables.
+#'
+#' @param table A table object from \code{table()}.
+#' @param margin Defaults to 1, calculate row-wise proportions. Change to 2 to calculate column-wise proportions.
+#'
+#' @return Returns a table with relative proportions/percentages appended to raw counts.
+#' @export
+#'
+#' @examples
+#' mydata <- data.frame(x1 = rep(c("Male","Female","Male"), 100), x2 = rep(c("High","Medium","Low"),100))
+#' # two way frequency table
+#' a <- table(mydata$x1,mydata$x2)
+#' prettytable(a)
+#' # one way frequency table
+#' a <- table(mydata$x1)
+#' prettytable(a)
+prettytable <- function(mytable, margin = 1)
+{
+  # account for one and two dimensional tables
+  if(length(dim(mytable)) == 2)
+  {
+    # calculate proportions
+    props <- prop.table(mytable, margin = margin)
+
+    for(r in 1:nrow(mytable)){
+      for(c in 1:ncol(mytable)){
+        mytable[r,c] <- paste0(mytable[r,c]," (",round(props[r,c],2) * 100,"%)")
+      }
+    }
+  } else if(length(dim(mytable)) == 1)
+  {
+    # calculate proportions
+    props <- prop.table(mytable)
+
+    for(r in 1:nrow(mytable)){
+      mytable[r] <- paste0(mytable[r]," (",round(props[r],2) * 100,"%)")
+    }
+  }
+
+  return(mytable)
+}
